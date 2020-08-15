95e70fe4c1f9d0c470890df694b4fb4474077e02
tisya207 <65344664+tisya207@users.noreply.github.com>
2020-08-15 10:38:46 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -40,7 +40,7 @@ function setup() {
 	engine = Engine.create();
 	world = engine.world;
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:true});
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.8, isStatic:true});
 	World.add(world, packageBody);
 	
 
